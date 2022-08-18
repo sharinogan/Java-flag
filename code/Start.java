466bbec4fe5ccee4dfc888bd1a4ad5debee344f1
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 11:12:39 +0700

    Check is values in array are sort

--- code/Start.java
+++ code/Start.java
@@ -1,14 +1,30 @@
 class Start {
     public static void main(String[] data) {
-        // Sqquential search
-        boolean found = false;
-        int city = 10500;
-        int[] area = { 10500, 10330, 10700, 11000, 12000 };
-        for(int i = 0; i < area.length; i++) {
-            if (city == area[i]) {
-                found = true;
+        int[] a = { 2, 1, 3, 4, 5, 9, 7 };  // No
+        int[] b = { 1, 2, 3, 4, 6, 8, 9 }; // Yes
+        boolean flag = true;
+        
+        // Check that is values in array are sort ot not.
+        // if it sort, print "Yes". then "No"
+        
+        // #1
+//        for (int i = 0; i < b.length - 1; i++) {
+//            if(b[i] <= b[i+1]) {
+//                
+//            }
+//            if (b[i] > b[i+1]) {
+//                flag = false;
+//            }
+//        }                  
+//        System.out.println(flag ? "Yes" : "No");
+
+        // #2
+        for(int i = 1; i < b.length; i++) {
+            if(b[i-1] <= b[i]) { }
+            if(b[i-1] > b[i]) {
+                flag = false;
             }
         }
-        System.out.println(found ? "Available" : "Not Available");
+        System.out.println(flag ? "Yes" : "No");
     }
 }
