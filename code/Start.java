515ee502991519817c61a8179b3744f3c6d340e1
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 11:02:12 +0700

    Sequential Search

--- code/Start.java
+++ code/Start.java
@@ -1,12 +1,14 @@
 class Start {
     public static void main(String[] data) {
-        int[] number = { 1, 2, 3, 4 };
-        boolean flag = true;
-        for(int i = 2; i < number.length; i++) {
-            if (number[i] % i == 0) { 
-              flag = false;               
-            }     
+        // Sqquential search
+        boolean found = false;
+        int city = 10500;
+        int[] area = { 10500, 10330, 10700, 11000, 12000 };
+        for(int i = 0; i < area.length; i++) {
+            if (city == area[i]) {
+                found = true;
+            }
         }
-        System.out.println(flag ? "Prime" : "Composite");
+        System.out.println(found ? "Available" : "Not Available");
     }
 }
